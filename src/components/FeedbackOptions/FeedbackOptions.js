1a507ea97b1112c65265f7bfda8f50cf145607d0
Igorippon <botvunaigor@gmail.com>
2023-08-26 15:49:53 +0300

    Update FeedbackOptions.js

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -3,9 +3,9 @@ import { Btn, List } from "./FeedbackOptions.styled";
 export const FeedbackOptions = ({ option, onLeaveFeedback }) => {
     return (
         <List>
-            <li><Btn name={option[0]} onClick={onLeaveFeedback}>Good</Btn></li>
-            <li><Btn name={option[1]} onClick={onLeaveFeedback}>Neutral</Btn></li>
-            <li><Btn name={option[2]} onClick={onLeaveFeedback}>Bad</Btn></li>
+            <li><Btn type="button" name={option[0]} onClick={onLeaveFeedback}>Good</Btn></li>
+            <li><Btn type="button" name={option[1]} onClick={onLeaveFeedback}>Neutral</Btn></li>
+            <li><Btn type="button" name={option[2]} onClick={onLeaveFeedback}>Bad</Btn></li>
         </List>
     );
 };
